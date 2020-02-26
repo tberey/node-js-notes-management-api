ee1e1fbc46e9e8268d88560bdc8d67914f39244f
60753515 <tomberey1@gmail.com>
2020-02-26 22:59:07 +0000

    V. Minor Updates - see README.md; entry 'Version 0.2.1 [2020-02-26]' for full details.

--- routes/post_routes.js
+++ routes/post_routes.js
@@ -3,10 +3,14 @@ module.exports = (app, db) => {
 
     app.post('/notes/create/new', (req, res) => { // Create: POST Request, to create a new record in db.
         
+        // Set parameters, if none submitted with POST Request.
+        req.body.title = req.body.title || 'New Note ' + Math.floor(Math.random() * 99999); // I.e. "?title=<someTitle>".
+        req.body.note = req.body.note || '[Auto-Placeholder] Enter details or a description of your note here...'; // I.e. "?note=<someTitle>".
+
         // Build new note as a object.
         const note = {
-            Title: req.query.title,
-            Note: req.query.note,
+            Title: req.body.title,
+            Note: req.body.note,
             Date: (new Date()).toDateString(),
             Time: `${(new Date()).getHours()}:${(new Date()).getMinutes()}`,
             ['Note Number']: Math.floor(Math.random() * 99999),
